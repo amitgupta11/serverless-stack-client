eb7fc5d07f6b950774e15125f8e9ccade6bbf619
Amit Gupta <mail@amitgupta.me>
2020-11-10 16:49:46 +0530

    Setting up React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: 'us-east-1',
+        BUCKET: 'notes-app-uploads-12433'
+    },
+    apiGateway: {
+        REGION: 'us-east-1',
+        URL: 'https://eqo4dttiui.execute-api.us-east-1.amazonaws.com/prod/'
+    },
+    cognito: {
+        REGION: 'us-east-1',
+        USER_POOL_ID: 'us-east-1_kg3OeOU6Z',
+        APP_CLIENT_ID: '26naf630632a4nmpsc6umhp3nn',
+        IDENTITY_POOL_ID: 'us-east-1:9003e189-6a41-4c75-af6b-eb8c76fbeb83'
+    }
+}
